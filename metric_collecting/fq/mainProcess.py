c1a13666969853856a8d290359413f551eb90652
open-mind <316257774@qq.com>
2018-07-25 12:49:38 +0800

    关掉pull

--- metric_collecting/fq/mainProcess.py
+++ metric_collecting/fq/mainProcess.py
@@ -44,7 +44,7 @@ def addSonarResult(issueNum,metrics,projId,repoName):
         conn.commit()
 
 def start():
-    pull.PullProcess()
+    # pull.PullProcess()
     sourcePathBase = os.getcwd() + "/" + cf.get("server", "gitCloneAddr")
     targetPathBase = os.getcwd() + "/" + cf.get("server", "sonarTempAddr")
     for repo in pull.getCloneRepos():
